1e2daa84ec188b00d0a2ff481a117e8bfec76a4e
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-05-21 15:59:11 +0500

    commit to task - 1

--- using_from_list/using_from_list/Program.cs
+++ using_from_list/using_from_list/Program.cs
@@ -1,11 +1,36 @@
-﻿namespace using_from_list
+﻿using System;
+using System.Collections.Generic;
+
+namespace using_from_list
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Hello, Guys! \n In this lesson we can learn how to use from list in C#! ");
-           ;
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player("Anvar"));
+            players.Add(new Player("MuhammadAli"));
+            players.Add(new Player("Quvonchbek"));
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+
+        class Player
+        {
+            public string username;
+
+            public Player(string username)
+            {
+                this.username = username;
+            }
+            public override string ToString()
+            {
+                return username;
+            }
         }
     }
 }
