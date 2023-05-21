f56a1a364ec2bfd55dc8156962e2c51354652104
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-05-21 15:55:27 +0500

    initial commit

---
+++ using_from_list/using_from_list/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace using_from_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello, Guys! \n In this lesson we can learn how to use from list in C#! ");
+           ;
+        }
+    }
+}
